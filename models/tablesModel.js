4515536cb1bc1515a8fb4182bcc99e71d0eb1144
Deepayan Nandy <deepayan.622@gmail.com>
2024-08-11 12:19:27 +0530

    customerId bug fix

--- models/tablesModel.js
+++ models/tablesModel.js
@@ -39,7 +39,7 @@ const tablesSchema= new mongoos.Schema({
                     },
                     customerId:{
                         type:String,
-                        required:true
+                        required:false
                     }
                 }
             ],
