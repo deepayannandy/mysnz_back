2ac3da020e9585c811d02ddef09c3ba27a5f446b
Deepayan Nandy <deepayan.622@gmail.com>
2024-10-11 17:57:57 +0530

    bugfix

--- routes/order.js
+++ routes/order.js
@@ -8,19 +8,21 @@ const productModel=require("../models/productModel")
 const customerModel=require("../models/customersModel")
 const orderHistoryModel= require("../models/orderHistoryModel")
 const verify_token= require("../validators/verifyToken")
+const historyModel= require("../models/historyModel")
+const customerHistoryModel= require("../models/customerHistoryModel")
 
 router.post("/",verify_token,async (req,res)=>{
     const loggedInUser= await userModel.findById(req.tokendata._id)
     if(!loggedInUser)return res.status(500).json({message: "Access Denied! Not able to validate the user."})
     const selectedStore= await storeModel.findById(loggedInUser.storeId)
     console.log(req.body)  
-    console.log(finalCustomerList)
+    const transId=selectedStore.storeName.replace(" ","").substring(0,3).toUpperCase()-selectedStore.transactionCounter
     const newOrderHistory= new orderHistoryModel({
         storeId:loggedInUser.storeId,
         date:new Date(),
         orderItems:req.body.orderItems,
-        customers: customer,
-        description:"Takeaway order",
+        customers: req.body.customer,
+        description:"Takeaway order "+req.body.paymentMethod,
         total:req.body.total,
         discount:req.body.discount,
         netPay:req.body.netPay,
@@ -28,9 +30,49 @@ router.post("/",verify_token,async (req,res)=>{
         transactionId:`${selectedStore.storeName.replace(" ","").substring(0,3).toUpperCase()}-${selectedStore.transactionCounter}`,
         credit:req.body.credit
     })
+    const newHistory= new historyModel({
+        storeId:loggedInUser.storeId,
+        date:new Date(),
+        customerName:req.body.customer.fullName,
+        description:"Takeaway order "+req.body.paymentMethod,
+        meal:req.body.netPay,
+        discount:req.body.discount,
+        netPay:req.body.netPay,
+        time:0,
+        booking:0,
+        status:req.body.netPay-req.body.cashIn>0?"Due":"Paid",
+        credit:req.body.netPay-req.body.cashIn<0?0:req.body.netPay-req.body.cashIn,
+        transactionId:transId,
+        empId:loggedInUser._id
+    })
+    const newCustomerHistory= new customerHistoryModel({
+        customerId:req.body.customer.customerId,
+        date:new Date(),
+        customerName:req.body.customer.fullName,
+        description:"Takeaway order "+req.body.paymentMethod,
+        quantity:0,
+        discount:req.body.discount,
+        netPay:req.body.netPay,
+        paid:req.body.netPay-req.body.cashIn<0?req.body.netPay:req.body.cashIn,
+        due:req.body.netPay-req.body.cashIn<0?0:req.body.netPay-req.body.cashIn,
+        transactionId:transId,
+        storeId:loggedInUser.storeId,
+        empId:loggedInUser._id
+    })
+    if(req.body.netPay-req.body.cashIn>0)
+        {
+        const pickedCustomer= await customerModel.findById(req.body.customer.customerId)
+        if(pickedCustomer)
+        {
+        console.log("I am called")
+        pickedCustomer.credit=pickedCustomer.credit+(req.body.netPay-req.body.cashIn)}
+        const updatedCustomer =await pickedCustomer.save()
+        }
     try{
-        const createdHistory= await newOrderHistory.save();
-        res.status(201).json({"_id":createdHistory.id})
+        const createdOrderHistory= await newOrderHistory.save();
+        const createdCustomerHistory= await newCustomerHistory.save();
+        const createdHistory= await newHistory.save();
+        res.status(201).json({"_id":createdOrderHistory.id})
     }catch(error){
         res.status(400).json({message:error.message})
     }
