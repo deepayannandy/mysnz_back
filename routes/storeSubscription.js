369745ce71bbca12295331c3e461dc91e933f6b9
Deepayan Nandy <deepayan.622@gmail.com>
2024-11-30 17:35:51 +0530

    storeSubscription renew and upgrade

--- routes/storeSubscription.js
+++ routes/storeSubscription.js
@@ -6,23 +6,33 @@ const storeSubscriptionModel= require("../models/storeSubscriptionModel")
 const storeModel=require("../models/storesModel")
 
 router.post("/",async(req,res)=>{
-    let store=await storeModel.findOne({_id:req.body.storeId});
+    let store=await storeModel.findById({_id:req.body.storeId});
     if(!store) return res.status(400).send({"message":"Store dose not exist!"});
-    // let subs=await subscriptionModel.findOne({_id:req.body.subscriptionId});
-    // if(!subs)res.status(500).json({"message":"Subscription dose not exist!"})
-
+    const activeSubs= await storeSubscriptionModel.findOne({$and:[{isActive:true},{storeId:req.body.storeId}]})
+    const selectedSubs= await subscriptionModel.findById(req.body.subscriptionId)
+    if(!selectedSubs) return res.status(400).send({"message":"Subscription dose not exist!"});
+    console.log(selectedSubs)
+    const StartDate= new Date()
+    const EndDate= new Date()
+    EndDate.setDate(EndDate.getDate() + selectedSubs.subscriptionValidity);
     const storeSubs= new storeSubscriptionModel({
         storeId:req.body.storeId,
-        isActive:req.body.isActive,
-        subscriptionName:req.body.subscriptionName,
+        isActive:true,
+        subscriptionName:selectedSubs.subscriptionName,
         subscriptionId:req.body.subscriptionId,
-        subscriptionValidity:req.body.subscriptionValidity,
-        startDate:req.body.startDate,
-        endDate:req.body.endDate,
-        isYearly:req.body.isYearly,
-        subscriptionAmount:req.body.subscriptionAmount
+        subscriptionValidity:selectedSubs.subscriptionValidity,
+        startDate:StartDate,
+        endDate:EndDate,
+        isYearly:selectedSubs.isYearly,
+        subscriptionAmount:selectedSubs.subscriptionPrice
     })
     try{
+        if(activeSubs){
+            activeSubs.isActive=false;
+            await activeSubs.save()
+        }
+        store.validTill=EndDate
+        await store.save()
         const newSSubs=await storeSubs.save()
         res.status(201).json({"_id":newSSubs.id})
     }
@@ -40,26 +50,6 @@ router.get("/:sid",async(req,res)=>{
         res.status(500).json({message: error.message})
     }
 })
-router.patch("/:sid",async(req,res)=>{
-    const selectedSubscription= await storeSubscriptionModel.findById(req.params.sid)
-    if(!selectedSubscription)
-        res.status(500).json({message: "Subscription not found"})
-    if(req.body.startDate!=null){
-        selectedSubscription.startDate=req.body.startDate;
-    }
-    if(req.body.endDate!=null){
-        selectedSubscription.endDate=req.body.endDate;
-    }
-    if(req.body.isActive!=null){
-        selectedSubscription.isActive=req.body.isActive;
-    }
-    try{
-        const updatedSub=await selectedSubscription.save();
-        res.status(201).json(updatedSub)
-    }
-    catch(error){
-        res.status(400).json({message:error.message})
-    }
-})
+
 
 module.exports=router
