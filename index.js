92e9e35e35e3ac4f2033ec4ac75c4eb626185b4a
deepayannandy <deepayan.622@gmail.com>
2025-04-16 21:19:18 +0530

    change for cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ db.on('error',(error)=> console.error(error))
 db.once('open',()=> console.log('Connected to Database!'))
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({origin:true}));
 
 const userRouter= require("./routes/user_auth")
 const storeRouter= require("./routes/store")
